fdc16c4bbb397afba6f4bc55622a1260e2eaaa42
Jia-Hsun Lo <57864575+Jason0102@users.noreply.github.com>
2024-05-17 14:43:42 +0800

    Update socket_client.py

--- python/socket_client.py
+++ python/socket_client.py
@@ -1,7 +1,6 @@
-# Socket Client
 import socket
 
-class socket_client():
+class SocketClient():
     def __init__(self, host_ip, port) -> None:
         self.host_ip = host_ip
         self.port = port
@@ -24,20 +23,8 @@ class socket_client():
             except:
                 continue
 
-def start_socket_client():
-    HOST = '140.112.14.225'  # 伺服器的 IP 地址
-    PORT = 12345        # 伺服器的端口號
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        message = "我不太游泳，要的話會在游泳池"
-        s.sendall(message.encode())
-
-        data = s.recv(1024)
-        print(f"Received from server: {data.decode()}")
-
 if __name__ == "__main__":
-    client = socket_client('140.112.14.225', 2468)
+    client = SocketClient('140.112.14.225', 2468)
     text = '佳勳' + '@@' + "我午餐吃了水餃"
     client.send_msg(text)
     print("wait for msg")
